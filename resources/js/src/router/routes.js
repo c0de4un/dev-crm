8ef2425db6083200db908ab3ad288ad4c1999476
c0de4un <code4un@yandex.ru>
2025-06-01 18:55:22 +0400

    Add empty auth page

--- resources/js/src/router/routes.js
+++ resources/js/src/router/routes.js
@@ -1,6 +1,14 @@
 const routes = [
   {
     path: '/',
+    component: () => import('layouts/AuthLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/Auth.vue') }
+    ]
+  },
+
+  {
+    path: '/dashboard',
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') }
